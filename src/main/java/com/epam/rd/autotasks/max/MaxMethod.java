f738ca582dfdb998562c8a222f44c1f9d37e3e2f
Denys Kovika <d.v.kovika@gmail.com>
2022-07-14 12:48:37 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,20 @@
 package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
+
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+
+        int maxValue = values[0];
+        for (int value : values) {
+            if (maxValue < value) {
+                maxValue = value; }
+            }
+        return maxValue;
+
+
     }
+
+
+
 }
